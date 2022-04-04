c7b4ae647f302c50a23f3027427dadec79f3660e
Majharul tanvir <mdtanvir6070@gmail.com>
2022-04-04 12:51:24 +0600

    Review

--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import useReview from '../../hooks/useReview';
 import image from '../../img/gsmarena_000.jpg'
 
 const Home = () => {
+    const [reviews] = useReview([])
+
     return (
         <div className='container'>
-            <div className='row my-5 justify-content-center align-items-center'>
+            <div className='row my-5 py-5 justify-content-center align-items-center'>
                 <div className="col-md-6 col-lg-6 col-xl-6">
                     <h4>Apple MacBook Air M1 2020</h4>
                     <h4>Chose your first gaming laptop</h4>
@@ -13,7 +16,26 @@ const Home = () => {
                 <div className="col-md-6 col-lg-6 col-xl-6 border-3">
                     <img className='w-100' src={image} alt="" />
                 </div>
-
+            </div>
+            <div>
+                <h3 className='text-center py-4'>Review</h3>
+                <div className='row row-cols-lg-2'>
+                    {
+                        reviews.map(review =>
+                            <div className="row border border-1">
+                                <div className="col-xl-4">
+                                    <img className='w-100' src={review.picture} alt="" />
+                                </div>
+                                <div className="col-xl-8">
+                                    <h6>Name: {review.name}</h6>
+                                    <p>{review.about}</p>
+                                </div>
+                            </div>)
+                    }
+                </div>
+                <div className='py-4 text-center'>
+                    <button className='py-2 px-5 rounded-3 border-0 my-4'>See all review</button>
+                </div>
             </div>
         </div>
     );
--- src/component/Review/Review.js
+++ src/component/Review/Review.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import useReview from '../../hooks/useReview';
 
 const Review = () => {
+    const [reviews] = useReview([])
     return (
-        <div>
-            
+        <div className='container my-5 py-3'>
+            <div className='row row-cols-xl-2 row-cols-1 row-cols-sm-2'>
+                {
+                    reviews.map(review => <div className="row align-items-center m-0 border border-2 py-2">
+                        <div className="col-xl-4">
+                            <img className='w-100' src={review.picture} alt="" />
+                        </div>
+                        <div className="col-xl-8">
+                            <h5>Name: {review.name}</h5>
+                            <p>{review.about}</p>
+                        </div>
+                    </div>)
+                }
+            </div>
         </div>
     );
 };
